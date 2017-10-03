587b86919ce3877dd17fb23cf3bdd635f5fddb86
nealalan <17957975+nealalan@users.noreply.github.com>
2017-10-02 23:48:34 -0500

    Create count.py

---
+++ count.py
@@ -0,0 +1,5 @@
+a = 0            # FIRST, set the initial value of the variable a to 0(zero).
+while a < 10:    # While the value of the variable a is less than 10 do the following:
+    a = a + 1    # Increase the value of the variable a by 1, as in: a = a + 1! 
+    print(a)     # Print to screen what the present value of the variable a is.
+                 # REPEAT! until the value of the variable a is equal to 9!? See note. 
